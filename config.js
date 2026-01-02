b2cfeaaa0e82bf49f717e91d44140c8f407ddb66
templecafe <thehindutemplestl@gmail.com>
2026-01-02 12:11:55 -0600

    login.js fix3 templecafe-repo

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-// config.js
+// this is config.js
 const SUPABASE_URL = "https://vupslmfufhcyqhqnscka.supabase.co";
 const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ1cHNsbWZ1ZmhjeXFocW5zY2thIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjczMTk4MDEsImV4cCI6MjA4Mjg5NTgwMX0.bcysv3u6GpA8_h7b4BD0U93blfXdDFg_1f4GRZTD91c";
 
