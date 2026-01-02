be52954eec84cdb69d0f489ff0e9a9c569c6b8be
templecafe <thehindutemplestl@gmail.com>
2026-01-02 10:58:32 -0600

    config anon key updated templecafe-repo

--- config.js
+++ config.js
@@ -2,4 +2,10 @@
 const SUPABASE_URL = "https://vupslmfufhcyqhqnscka.supabase.co";
 const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ1cHNsbWZ1ZmhjeXFocW5zY2thIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjczMTk4MDEsImV4cCI6MjA4Mjg5NTgwMX0.bcysv3u6GpA8_h7b4BD0U93blfXdDFg_1f4GRZTD91c";
 
-window.supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+window.supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
+  auth: {
+    persistSession: true,
+    autoRefreshToken: true,
+    detectSessionInUrl: true
+  }
+});
